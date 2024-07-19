0e32c2320c9c98db67b8d73b527a19b7cd8261cb
bhuwantechnial <techinicalbhuwan@gmail.com>
2024-07-19 16:33:50 +0545

    hehe

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: "bhuwan",
+    githubLink: "https://github.com/bhuwantechnical" ,
+    email : "technicalbhuwan@gmail.com",
+    hobbies : [
+        "Movies",
+        "Travelling",
+        "Football"
+    ],
+    jobTitle : "Software Engineer",
+    degree: "Bachelor's in Computer Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali,English",
+}
+export default data;
